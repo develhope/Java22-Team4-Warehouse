e0a8f4a5f7ae9e2238408b0a54db2bbf0ebe129f
halaaniba <156901595+halaaniba@users.noreply.github.com>
2024-02-01 15:15:53 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Smartphone smartphone1= new Smartphone("smartphone", "Samsung", "Galaxy S20",
                 "Ultimo modello di Samsung", 6.2, 128, 799.99, 999.99, 12345);
-
+        Notebook notebook1 = new Notebook("Notebook","HP","hp pro123","" +
+                "un notebook di ultima generazione",12.7,223,207.88,370.30,4);
         System.out.println(smartphone1);
+        System.out.println(notebook1);
 
     }
 }
