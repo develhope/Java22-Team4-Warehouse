d8690043837389982e62c8bc6d8c5f2b2e437c28
halaaniba <156901595+halaaniba@users.noreply.github.com>
2024-02-01 15:36:17 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ public class Main {
                 "Ultimo modello di Samsung", 6.2, 128, 799.99, 999.99, 12345);
         Notebook notebook1 = new Notebook("Notebook","HP","hp pro123","" +
                 "un notebook di ultima generazione",12.7,223,207.88,370.30,4);
+        Tablet tablet1 = new Tablet("Tablet","apple","SR123","è un tablet funzionale di ulrima generazione",
+                12,300,800.99,978.78,9);
         System.out.println(smartphone1);
         System.out.println(notebook1);
+        System.out.println(tablet1);
 
     }
 }
---
+++ src/Tablet.java
@@ -0,0 +1,102 @@
+public class Tablet {
+
+    private String tipoDispositivo;
+    private String produttore;
+    private String modello;
+    private String descrizione;
+    private double dimensioneDisplay;
+    private double dimensioneArchiviazione;
+    private double prezzoAcquisto;
+    private double prezzoVendita;
+    private int idDispositivo;  // Id univoco del dispositivo nel magazzino
+
+    public Tablet(String tipoDispositivo, String produttore, String modello, String descrizione,
+                  double dimensioneDisplay, double dimensioneArchiviazione, double prezzoAcquisto,
+                  double prezzoVendita, int idDispositivo) {
+        this.tipoDispositivo = tipoDispositivo;
+        this.produttore = produttore;
+        this.modello = modello;
+        this.descrizione = descrizione;
+        this.dimensioneDisplay = dimensioneDisplay;
+        this.dimensioneArchiviazione = dimensioneArchiviazione;
+        this.prezzoAcquisto = prezzoAcquisto;
+        this.prezzoVendita = prezzoVendita;
+        this.idDispositivo = idDispositivo;
+    }
+
+    // Metodi getter e setter per gli attributi
+    public String getTipoDispositivo() {
+        return tipoDispositivo;
+    }
+
+    public void setTipoDispositivo(String tipoDispositivo) {
+        this.tipoDispositivo = tipoDispositivo;
+    }
+
+    public String getProduttore() {
+        return produttore;
+    }
+
+    public void setProduttore(String produttore) {
+        this.produttore = produttore;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getDimensioneDisplay() {
+        return dimensioneDisplay;
+    }
+
+    public void setDimensioneDisplay(double dimensioneDisplay) {
+        this.dimensioneDisplay = dimensioneDisplay;
+    }
+
+    public double getDimensioneArchiviazione() {
+        return dimensioneArchiviazione;
+    }
+
+    public void setDimensioneArchiviazione(double dimensioneArchiviazione) {
+        this.dimensioneArchiviazione = dimensioneArchiviazione;
+    }
+
+    public double getPrezzoAcquisto() {
+        return prezzoAcquisto;
+    }
+
+    public void setPrezzoAcquisto(double prezzoAcquisto) {
+        this.prezzoAcquisto = prezzoAcquisto;
+    }
+
+    public double getPrezzoVendita() {
+        return prezzoVendita;
+    }
+
+    @Override
+    public String toString() {
+        return "Tablet{" +
+                "tipoDispositivo='" + tipoDispositivo + '\'' +
+                ", produttore='" + produttore + '\'' +
+                ", modello='" + modello + '\'' +
+                ", descrizione='" + descrizione + '\'' +
+                ", dimensioneDisplay=" + dimensioneDisplay +
+                ", dimensioneArchiviazione=" + dimensioneArchiviazione +
+                ", prezzoAcquisto=" + prezzoAcquisto +
+                ", prezzoVendita=" + prezzoVendita +
+                ", idDispositivo=" + idDispositivo +
+                '}';
+    }
+}
