b1c4d2a06cca4b7694d57c04c898d59b1c12ef93
danieleocchi <dennyais146@gmail.com>
2024-02-14 17:15:48 +0100

    commit per dispositivo notebook tablet smartphone

---
+++ src/Dispositivi/Smartphone.java
@@ -0,0 +1,7 @@
+package Dispositivi;
+
+public class Smartphone extends Dispositivi{
+    public Smartphone(String dispositivo, String brand, int display, String modello, String descrizione, double prezzo, double memoria, double prezzoAcquisto) {
+        super(dispositivo, brand, display, modello, descrizione, prezzo, memoria, prezzoAcquisto);
+    }
+}
---
+++ src/Dispositivi/Tablet.java
@@ -0,0 +1,7 @@
+package Dispositivi;
+
+public class Tablet extends Dispositivi{
+    public Tablet(String dispositivo, String brand, int display, String modello, String descrizione, double prezzo, double memoria, double prezzoAcquisto) {
+        super(dispositivo, brand, display, modello, descrizione, prezzo, memoria, prezzoAcquisto);
+    }
+}
