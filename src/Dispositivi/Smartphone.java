5cd610ac2a7a85c18ca26cf487c70e354ef8502e
Jamming <j.lucreziaparrillo@gmail.com>
2024-02-19 21:16:58 +0100

    magazzino

--- src/Dispositivi/Smartphone.java
+++ src/Dispositivi/Smartphone.java
@@ -2,6 +2,6 @@ package Dispositivi;
 
 public class Smartphone extends Dispositivi{
     public Smartphone(String dispositivo, String brand, int display, String modello, String descrizione, double prezzo, double memoria, double prezzoAcquisto) {
-        super(dispositivo, brand, display, modello, descrizione, prezzo, memoria, prezzoAcquisto);
+        super(dispositivo, brand, String.valueOf(display), modello, descrizione, prezzo, memoria, prezzoAcquisto);
     }
 }
--- src/Dispositivi/Tablet.java
+++ src/Dispositivi/Tablet.java
@@ -2,6 +2,6 @@ package Dispositivi;
 
 public class Tablet extends Dispositivi{
     public Tablet(String dispositivo, String brand, int display, String modello, String descrizione, double prezzo, double memoria, double prezzoAcquisto) {
-        super(dispositivo, brand, display, modello, descrizione, prezzo, memoria, prezzoAcquisto);
+        super(dispositivo, brand, String.valueOf(display), modello, descrizione, prezzo, memoria, prezzoAcquisto);
     }
 }
