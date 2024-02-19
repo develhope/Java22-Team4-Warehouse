216c73c270315088a0f6b3ff3c09af2b77b85e4c
Jamming <j.lucreziaparrillo@gmail.com>
2024-02-19 19:40:15 +0100

    magazzino

--- src/Magazzino.java
+++ src/Magazzino.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class Magazzino {
 
     private List<Dispositivi> listaDispositivi = new ArrayList<>();
-
+    //un metodo che aggiunge i dispositivi a magazzino
     public void aggiungiDispositivoMagazzino(Dispositivi dispositivi){
         listaDispositivi.add(dispositivi);
     }
-
+    //Un metodo che stampa i dispositivi del magazzino
     public List<Dispositivi> stampaDispositivi(List<Dispositivi> listaDispositivi){
         for(Dispositivi dispositivo: listaDispositivi){
             if(dispositivo.getDispositivo().equals(null)){
@@ -19,25 +19,25 @@ public class Magazzino {
         }
         return listaDispositivi;
     }
-
+    //Un metodo che esegue una ricerca per produttore e genera un messaggio se il dispositivo non è presente
     public void ricercaProduttore(List<Dispositivi> listaDispositivi){
         for (Dispositivi dispositivo : listaDispositivi){
-            if (dispositivo.getBrand().equals(null)){
+            if (dispositivo.getBrand() == null){
                 System.out.println("il dispositivo non è disponibile");
             }
             System.out.println(dispositivo.getBrand());
         }
     }
+    //Un metodo che esegue una ricerca per produttore e genera un messaggio se il dispositivo non è presente
     public void ricercaModello(List<Dispositivi> listaDispositivi) {
         for (Dispositivi dispositivo : listaDispositivi) {
-            if (dispositivo.getModello().equals(null)) {
+            if (dispositivo.getModello() == null) {
                 System.out.println("il dispositivo non è disponibile");
             }
             System.out.println(dispositivo.getModello());
         }
     }
         /* Un metodo che permetta la ricerca per prezzo di vendita
-          Dovrà resitutire la lista di dispositivi frutto della ricerca
           o un errore nel caso in cui la ricerca non produca risultati.*/
         public  ArrayList<Dispositivi> cercaPerPrezzo(ArrayList<Dispositivi> listaDispositivi, double prezzoDaCercare){
             ArrayList<Dispositivi> dispositiviRicerca = new ArrayList<>();
