c217c41926e42d552990b6b5848bfa546e3f0f0f
martacaponnetto <martacaponnetto@yahoo.it>
2024-02-19 18:33:51 +0100

    first commit

--- src/Magazzino.java
+++ src/Magazzino.java
@@ -28,12 +28,66 @@ public class Magazzino {
             System.out.println(dispositivo.getBrand());
         }
     }
-    public void ricercaModello(List<Dispositivi> listaDispositivi){
-        for (Dispositivi dispositivo : listaDispositivi){
-            if (dispositivo.getModello().equals(null)){
+    public void ricercaModello(List<Dispositivi> listaDispositivi) {
+        for (Dispositivi dispositivo : listaDispositivi) {
+            if (dispositivo.getModello().equals(null)) {
                 System.out.println("il dispositivo non è disponibile");
             }
             System.out.println(dispositivo.getModello());
         }
     }
+        /* Un metodo che permetta la ricerca per prezzo di vendita
+          Dovrà resitutire la lista di dispositivi frutto della ricerca
+          o un errore nel caso in cui la ricerca non produca risultati.*/
+        public  ArrayList<Dispositivi> cercaPerPrezzo(ArrayList<Dispositivi> listaDispositivi, double prezzoDaCercare){
+            ArrayList<Dispositivi> dispositiviRicerca = new ArrayList<>();
+            for (Dispositivi dispositivo : listaDispositivi) {
+                if (dispositivo.getPrezzo() == prezzoDaCercare) {
+                    dispositiviRicerca.add(dispositivo);
+                }
+            }
+            return dispositiviRicerca;
+        }
+    public ArrayList<Dispositivi> cercaPerPrezzoAcquisto(ArrayList<Dispositivi> listaDispositivi, double prezzoAcquistoDaCercare) {
+        ArrayList<Dispositivi> dispositiviRicerca = new ArrayList<>();
+        for (Dispositivi dispositivo : listaDispositivi) {
+            if (dispositivo.getPrezzo() == prezzoAcquistoDaCercare) {
+                dispositiviRicerca.add(dispositivo);
+            }
+        }
+        return dispositiviRicerca;
+    }
+    public  ArrayList<Dispositivi> cercaPerRangePrezzo(ArrayList<Dispositivi> listaDispositivi, double prezzoMin, double prezzoMax){
+        ArrayList<Dispositivi> dispotiviDiRicerca = new ArrayList<>();
+        for(Dispositivi dispositivo : listaDispositivi) {
+            double prezzoDispotivo = dispositivo.getPrezzo();
+            if (prezzoDispotivo >= prezzoMin && prezzoDispotivo <= prezzoMax) {
+                dispotiviDiRicerca.add(dispositivo);
+            }
+        }
+        return dispotiviDiRicerca;
+
+
+    }
+    public static double calcolaSpesaMedia(ArrayList<Dispositivi> listaDispositivi){
+        if (listaDispositivi.isEmpty()) {
+            return 0.0;
+        }
+        double totaleSpesa = 0;
+        for (Dispositivi dispositivo : listaDispositivi) {
+            totaleSpesa += dispositivo.getPrezzo();
+        }
+
+        return totaleSpesa / listaDispositivi.size();
+
+    }
 }
+
+
+
+
+
+
+
+
+
