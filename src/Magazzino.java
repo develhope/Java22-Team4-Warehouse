a7829822f3030c2fb0a7665cbddb9def73cb5317
Jamming <j.lucreziaparrillo@gmail.com>
2024-02-20 18:08:59 +0100

    magazzino scanner update

--- src/Magazzino.java
+++ src/Magazzino.java
@@ -2,13 +2,37 @@ import Dispositivi.Dispositivi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Magazzino {
 
     protected List<Dispositivi> listaDispositivi = new ArrayList<>();
 
-    public void aggiungiDispositivoMagazzino(Dispositivi dispositivi){
-        listaDispositivi.add(dispositivi);
+    public void aggiungiDispositivoMagazzino(Scanner scanner1){
+
+        Scanner scanner = new Scanner(System.in);
+        Dispositivi dispositivo = new Dispositivi();
+        System.out.println("Ciao magazziniere! Inserisci il tipo di dispositivo che vuoi inserire...");
+        dispositivo.setTipoDispositivo(scanner.nextLine());
+        System.out.println("Digita l'ID del tuo dispositivo.....");
+        dispositivo.setId(Long.parseLong(scanner.nextLine()));
+        System.out.println("Digita il modello del dispositivo.....");
+        dispositivo.setModello(scanner.nextLine());
+        System.out.println("Digita il produttore del dispositivo.....");
+        dispositivo.setBrand(scanner.nextLine());
+        System.out.println("Digita la descrizione del dispositivo.....");
+        dispositivo.setDescrizione(scanner.nextLine());
+        System.out.println("Digita la dimensione del display....");
+        dispositivo.setDisplay(String.valueOf(Integer.parseInt(scanner.nextLine())));
+        System.out.println("Digita la dimensione dello spazio di archiviazione del dispositivo....");
+        dispositivo.setMemoria(Integer.parseInt(scanner.nextLine()));
+        System.out.println("Digita il prezzo di acquisto.....");
+        dispositivo.setPrezzoAcquisto(Double.parseDouble(scanner.nextLine()));
+        System.out.println("Digita il prezzo di vendita.....");
+        dispositivo.setPrezzo(Double.parseDouble(scanner.nextLine()));
+        System.out.println("Grazie! A presto");
+        scanner.close();
+        listaDispositivi.add(dispositivo);
     }
 
     public List<Dispositivi> stampaDispositivi(List<Dispositivi> listaDispositivi){
--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Magazzino magazzino = new Magazzino();
-        Scanner scanner = new Scanner(System.in);
-        Dispositivi dispositivo = new Dispositivi(scanner);
-
-        System.out.println("Ciao magazziniere! Inserisci il tipo di dispositivo che vuoi inserire...");
-        dispositivo.setTipoDispositivo(scanner.nextLine());
-        System.out.println("Digita l'ID del tuo dispositivo.....");
-        dispositivo.setId(Long.parseLong(scanner.nextLine()));
-        System.out.println("Digita il modello del dispositivo.....");
-        dispositivo.setModello(scanner.nextLine());
-        System.out.println("Digita il produttore del dispositivo.....");
-        dispositivo.setBrand(scanner.nextLine());
-        System.out.println("Digita la descrizione del dispositivo.....");
-        dispositivo.setDescrizione(scanner.nextLine());
-        System.out.println("Digita la dimensione del display....");
-        dispositivo.setDisplay(String.valueOf(Integer.parseInt(scanner.nextLine())));
-        System.out.println("Digita la dimensione dello spazio di archiviazione del dispositivo....");
-        dispositivo.setMemoria(Integer.parseInt(scanner.nextLine()));
-        System.out.println("Digita il prezzo di acquisto.....");
-        dispositivo.setPrezzoAcquisto(Double.parseDouble(scanner.nextLine()));
-        System.out.println("Digita il prezzo di vendita.....");
-        dispositivo.setPrezzo(Double.parseDouble(scanner.nextLine()));
-        System.out.println("Grazie! A presto");
-        scanner.close();
-
-        magazzino.aggiungiDispositivoMagazzino(dispositivo);
-        magazzino.stampaDispositivi(magazzino.listaDispositivi);
-
+        magazzino.aggiungiDispositivoMagazzino(new Scanner(System.in));
+        System.out.println(magazzino.stampaDispositivi(magazzino.listaDispositivi));
     }
 }
